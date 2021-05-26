687033d3903d8946253eaf6482b5dbf62333b4a6
daniel <daniel@aiteam.pl>
2021-05-26 16:16:43 +1000

    Step 2 - Layout

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="header">
+   <div class="container">
+      <div class="row">
+        <div class="col">
+          <Logo />
+        </div>
+    </div>
+   </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 2rem auto;
+  padding-bottom: 2rem;
+  border-bottom: 1px solid #ccc;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
